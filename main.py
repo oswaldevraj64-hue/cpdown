070ee679c7afd3ceba880dc47030759ad1c6f245
oswaldevraj64-hue <oswaldevraj64@gmail.com>
2026-02-14 01:21:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -230,4 +230,10 @@ async def account_login(bot: Client, m: Message):
     await m.reply_text("Done")
 
 
-bot.run()
+from pyrogram import idle
+
+bot.start()
+print("Bot Started 🔥")
+idle()
+bot.stop()
+
